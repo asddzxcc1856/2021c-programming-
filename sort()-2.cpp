32d199319807e0d33f4c945598721be7b1310150
asddzxcc1856 <59413200+asddzxcc1856@users.noreply.github.com>
2021-04-27 22:32:41 +0800

    Add files via upload

---
+++ sort()-2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>  // 示範 利用排序可以做的變化資料型態 通稱 => 多鍵值排序 
+#include <algorithm>
+using namespace std;
+
+bool cmp(pair<int,int> a,pair<int,int> b)
+{
+   return a.second > b.second;
+}
+
+bool cmp2(pair<int,int> a,pair<int,int> b)
+{
+   return a.second < b.second;
+}
+
+int main ()
+{
+   //宣告沒排序的p陣列 
+   pair<int,int> p[5]={{1,30},{2,40},{3,20},{4,90},{5,80}};//第一項 : 學生編號 . 第二項 : 學生分數 
+   
+   
+   //============================================================================================================
+   
+   
+   //排序函式sort(array address , array address + 長度); 
+   sort(p,p+5); 
+   
+   
+   //輸出 
+   cout << "排序後結果 : \n";
+   for (int i=0;i<5;i++)
+   {
+      cout << p[i].first << " " << p[i].second << "\n";
+   }
+   
+   
+   //============================================================================================================
+   
+   
+   //排序函式sort(array address , array address + 長度 , 比較函式); 
+   sort(p,p+5,cmp);//由高分到低分 
+   
+   
+   //輸出 
+   cout << "排序後結果 : \n";
+   for (int i=0;i<5;i++)
+   {
+      cout << p[i].first << " " << p[i].second << "\n";
+   }
+   
+   
+   //============================================================================================================
+   
+   
+   //排序函式sort(array address , array address + 長度 , 比較函式); 
+   sort(p,p+5,cmp2);//由低分到高分 
+   
+   
+   //輸出 
+   cout << "排序後結果 : \n";
+   for (int i=0;i<5;i++)
+   {
+      cout << p[i].first << " " << p[i].second << "\n";
+   }
+
+}
