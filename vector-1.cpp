8fb1689d4e9f70828b703bd6a3b8e0f79ef0f151
asddzxcc1856 <59413200+asddzxcc1856@users.noreply.github.com>
2021-04-27 23:20:23 +0800

    Add files via upload

---
+++ vector-1.cpp
@@ -0,0 +1,39 @@
+#include <iostream> // ¤Gºûvector
+#include <vector>
+using namespace std;
+
+int main ()
+{
+   vector<vector<int> > v;
+   for (int i=0;i<3;i++)
+   {
+      vector<int> v2;
+      for (int j=0;j<3;j++)
+      {
+         int a;
+         
+         cin >> a;
+         v2.push_back(a);
+      
+      }
+      v.push_back(v2);
+   }
+   
+   for (int i=0;i<3;i++)
+   {
+      for (int j=0;j<3;j++)
+      {
+         cout << v[i][j] << " ";
+      }
+      cout << "\n";
+   }
+   
+}
+
+/*
+
+1 3 5
+7 9 10
+11 61 4
+
+*/
