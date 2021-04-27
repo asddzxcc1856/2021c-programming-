67b073cd4e5cdba9759079a3346bed0423d119cb
asddzxcc1856 <59413200+asddzxcc1856@users.noreply.github.com>
2021-04-27 22:11:23 +0800

    Add files via upload

---
+++ sort()-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main ()
+{
+   //宣告沒排序的c陣列 
+   int c[5] = {5,3,4,2,1,36,4};
+   
+   //排序函式sort(array address,array address + 長度); 
+   sort(c,c+5);
+   
+   //輸出 
+   cout << "排序後結果 : "
+   for (int i=0;i<5;i++)
+   {
+      cout << c[i] << " ";
+   }
+   
+}
