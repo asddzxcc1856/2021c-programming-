e092c27c1690a912b9747256b1e4ad4a394e661c
嚕梯 <59413200+asddzxcc1856@users.noreply.github.com>
2021-05-01 22:02:05 +0800

    Update 二分搜尋法.cpp

--- 二分搜尋法.cpp
+++ 二分搜尋法.cpp
@@ -6,17 +6,17 @@ int binary_search(int arr[20],int L,int R,int index)
 {
    if(L<=R)
    {
-      int m = (L + R) / 2;
+      int m = (L + R) / 2; //每次都砍一半找
       
-      if(arr[m]==index)
+      if(arr[m]==index) //如果找到就回傳位置m
       {
          return m;
       }
-      else if(arr[m]>index)
+      else if(arr[m]>index) //如果發現目前的位置比要找的數字大就把範圍變成 L~m-1 => 因為陣列的數字是由小排到大 所以要往左邊找
       {
          return binary_search(arr,L,m-1,index);
       }
-      else if(arr[m]<index)
+      else if(arr[m]<index) //如果發現目前的位置比要找的數字小就把範圍變成 m+1~R => 因為陣列的數字是由小排到大 所以要往右邊找
       {
          return binary_search(arr,m+1,R,index);
       }
